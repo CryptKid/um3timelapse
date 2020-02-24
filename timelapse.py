3a4f7ff4f0f0a6ca2ba9f94eeb649c768a3a16b9
Zehka <cryptkid@noreply.github.com>
2020-02-25 00:59:34 +0100

    made script run as loop

--- timelapse.py
+++ timelapse.py
@@ -13,10 +13,14 @@ cliParser.add_argument('HOST', type=str,
 			help='IP address of the Ultimaker 3')
 cliParser.add_argument('DELAY', type=float,
 			help='Time between snapshots in seconds')
-cliParser.add_argument('OUTFILE', type=str,
-			help='Name of the video file to create. Recommended formats are .mkv or .mp4.')
+cliParser.add_argument('FOLDER', nargs='?', type=str,
+			help='Folder of the video files to create. Optional')
 options = cliParser.parse_args()
-
+FOLDER=""
+if (options.FOLDER != None):
+    FOLDER = options.FOLDER
+    if not(FOLDER.endswith('/')):
+            FOLDER = FOLDER + '/'
 imgurl = "http://" + options.HOST + ":8080/?action=snapshot"
 
 api = Ultimaker3(options.HOST, "Timelapse")
@@ -49,6 +53,19 @@ def progress():
 			return "%05.2f %%" % (p)
 		except exceptions.ConnectionError as err:
 			print_error(err)
+def createOutName():
+    tempOutName=""
+    # If the printer gets disconnected, retry indefinitely
+    while tempOutName=="":
+        try:
+            print_job = api.get("api/v1/print_job").json()
+            print_job_name = print_job.get("name")
+            print_job_uuid = print_job.get("uuid")
+            tempOutName = (print_job_name + "__" + print_job_uuid + ".mp4")
+            return tempOutName
+        except exceptions.ConnectionError as err:
+            tempOutName = ""
+            print_error(err) 
 
 def print_error(err):
 	print("Connection error: {0}".format(err))
@@ -56,33 +73,36 @@ def print_error(err):
 	print()
 	sleep(1)
 
-tmpdir = mkdtemp()
-filenameformat = os.path.join(tmpdir, "%05d.jpg")
-print(":: Saving images to",tmpdir)
+while (True):
+    tmpdir = mkdtemp()
+    filenameformat = os.path.join(tmpdir, "%05d.jpg")
+    print(":: Saving images to",tmpdir)
 
-if not os.path.exists(tmpdir):
-	os.makedirs(tmpdir)
+    if not os.path.exists(tmpdir):
+        os.makedirs(tmpdir)
 
-print(":: Waiting for print to start")
-while not printing():
-	sleep(1)
-print(":: Printing")
+    print(":: Waiting for print to start")
+    while not printing():
+        sleep(1)
+    print(":: Printing")
+    print(":: Fetching name")
+    OUTFILE = FOLDER +  createOutName()
+    print(":: Will be saved as " + OUTFILE)
+    count = 0
 
-count = 0
+    while printing():
+        count += 1
+        response = urlopen(imgurl)
+        filename = filenameformat % count
+        f = open(filename,'bw')
+        f.write(response.read())
+        f.close
+        print("Print progress: %s Image: %05i" % (progress(), count), end='\r')
+        sleep(options.DELAY)
 
-while printing():
-	count += 1
-	response = urlopen(imgurl)
-	filename = filenameformat % count
-	f = open(filename,'bw')
-	f.write(response.read())
-	f.close
-	print("Print progress: %s Image: %05i" % (progress(), count), end='\r')
-	sleep(options.DELAY)
-
-print()
-print(":: Print completed")
-print(":: Encoding video")
-ffmpegcmd = "ffmpeg -r 30 -i " + filenameformat + " -vcodec libx264 -preset veryslow -crf 18 " + options.OUTFILE
-print(ffmpegcmd)
-os.system(ffmpegcmd)
+    print()
+    print(":: Print completed")
+    print(":: Encoding video")
+    ffmpegcmd = "ffmpeg -r 30 -i " + filenameformat + " -vcodec libx264 -preset veryslow -crf 18 " + OUTFILE
+    print(ffmpegcmd)
+    os.system(ffmpegcmd)
